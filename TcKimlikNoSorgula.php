388878a4809524bcf7d97c564549f76aaf809c3b
Kadir Emin İslamoğlu <keislamoglu@users.noreply.github.com>
2017-05-16 20:14:46 +0300

    the method "dogrula" added

--- TcKimlikNoSorgula.php
+++ TcKimlikNoSorgula.php
@@ -1,16 +1,15 @@
 <?php
 /**
- * @author Kadir Emin İslamoğlu <keislamoglu@yandex.com>
+ * @author Kadir Emin İslamoğlu
  * @date 2015-08-03
  * @url <https://github.com/keislamoglu>
  */
-
 class TcKimlikNoSorgula {
     private $tcKimlikNo;
     private $ad;
     private $soyad;
     private $dogumYili;
-
+    
     /**
      * TcKimlik No
      * @param $tcKimlikNo
@@ -56,6 +55,16 @@ class TcKimlikNoSorgula {
     }
 
     /**
+     * Büyük harflere dönüştürme
+     * @param $string
+     * @return string
+     */
+    private function upperCase($string) {
+        $string = str_replace(array('i'), array('İ'), $string);
+        return mb_convert_case($string, MB_CASE_UPPER, "UTF-8");
+    }
+
+    /**
      * Sorgulama
      * @return bool
      * @throws \Exception
@@ -76,7 +85,6 @@ class TcKimlikNoSorgula {
                     </TCKimlikNoDogrula>
                 </soap:Body>
             </soap:Envelope>';
-
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, "https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx");
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -93,17 +101,36 @@ class TcKimlikNoSorgula {
         ));
         $response = curl_exec($ch);
         curl_close($ch);
-
         return strip_tags($response) == 'true';
     }
 
     /**
-     * Büyük harflere dönüştürme
-     * @param $string
-     * @return string
+     * Algoritmik olarak dogrulama yapar
+     * @return bool
      */
-    public function upperCase($string) {
-        $string = str_replace(array('i'), array('İ'), $string);
-        return mb_convert_case($string, MB_CASE_UPPER, "UTF-8");
+    public function dogrula()
+    {
+        // 11 karakterden oluşmalıdır
+        if (strlen($this->tcKimlikNo) != 11) {
+            return false;
+        }
+        // Rakamlardan oluşmalıdır
+        if (!preg_match('/(?<!\S)\d++(?!\S)/', $this->tcKimlikNo)) {
+            return false;
+        }
+        // Algoritmik hesaplamalar
+        $digit = preg_split('//', $this->tcKimlikNo, -1, PREG_SPLIT_NO_EMPTY);
+        if ($digit[0] == 0) {
+            return false;
+        }
+        $odd = $digit[0] + $digit[2] + $digit[4] + $digit[6] + $digit[8];
+        $even = $digit[1] + $digit[3] + $digit[5] + $digit[7];
+        $digit10 = ($odd * 7 - $even) % 10;
+        $total = ($odd + $even + $digit[9]) % 10;
+        if ($digit10 != $digit[9] or $total != $digit[10]) {
+            return false;
+        }
+
+        return true;
     }
 } 
